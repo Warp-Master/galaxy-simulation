6739d857026344f4de85204e78dd03ecd555e466
Dmitrii Chernov <2003dima@gmail.com>
2022-12-14 22:11:12 +0300

    Fix updateSize() in Star.cpp

--- Star.cpp
+++ Star.cpp
@@ -12,7 +12,7 @@ const int starColors[] = {0x264653,
                           0xf4a261,
                           0xe76f51,
                           0xe63946};
-const int nColor = sizeof(starColors) / sizeof(starColors[0]);
+const int nBorders = sizeof(borderMass) / sizeof(borderMass[0]);
 
 Star::Star(const double *coord, const double *velocity, double mass): col(0xffffff) {
     for (int k = 0; k < dim; ++k) {
@@ -36,19 +36,14 @@ Star& Star::operator+=(const Star &rhs) {
 
 void Star::updateColor() {
     int i = 0;
-    while (borderMass[i] < m && i + 1 < nColor) ++i;
+    while (i + 1 < nBorders && borderMass[i] < m ) ++i;
     col = starColors[i];
 }
 
 void Star::updateSize() {
-    double mRatio = m / massSun;
-    size = floor(exp(mRatio) + 2);
-//    double mRatio2 = mRatio * mRatio;
-//    double mRatio3 = mRatio2 * mRatio;
-//    double rRatio = (0.00154297190393498651*mRatio3) - (0.06457707406352142243*mRatio2) + (0.98859893296201306612*mRatio) + 0.05502363603633853018;
-//    size = std::floor(sunSize * 2 * rRatio);
-//
-//    int i = 0;
-//    while (borderMass[i] < m && i + 1 < nColor) ++i;
-//    size = (i+1)*2;
+    int i = 0;
+    while (i + 1 < nBorders && borderMass[i] < m) ++i;
+    if (floor(m / massSun) > 3) i += 3;
+    else i += (int)(m / massSun);
+    size = i + 1;
 }
